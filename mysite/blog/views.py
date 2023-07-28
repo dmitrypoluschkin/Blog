ac721660436184d40eed44d1411c6765d1a363ea
Dima Polushkin <119305773+dmitrypoluschkin@users.noreply.github.com>
2023-07-28 21:50:34 +0400

    blog
    blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
 def post_list(request):
     post_list = Post.published.all()
     # постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    page_number = request.GET.get('page')
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
@@ -26,3 +35,13 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
