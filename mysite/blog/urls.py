8eb6b1b3e5588bd63f191fe972a75dc565706473
Dima Polushkin <119305773+dmitrypoluschkin@users.noreply.github.com>
2023-08-12 15:19:27 +0400

    blofg

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog'
@@ -20,5 +21,8 @@ urlpatterns = [
          name='post_share'),
     path('<int:post_id>/comment/',
          views.post_comment,
-         name='post_comment')
+         name='post_comment'),
+    path('feed/', LatestPostsFeed(),
+         name='post_feed'),
+
 ]
