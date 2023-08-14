27ebcfeed20f6c2febb208bb3e89a7a9944f80cf
Dima Polushkin <119305773+dmitrypoluschkin@users.noreply.github.com>
2023-08-14 16:00:12 +0400

    blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,6 +9,8 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, \
+    SearchQuery, SearchRank
 
 
 def post_list(request, tag_slug=None):
@@ -134,8 +136,17 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
 
-    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query, config='spanish')
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
+
+    return render(request, 'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results}
+                  )
